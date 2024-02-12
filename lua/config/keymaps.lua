925038f984778f9482243a7265a727f714c35688
Travis <travis@netronline.com>
2024-02-11 18:47:20 -0700

    fixed annoying autocomplete

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Add any additional keymaps hereby
+--
+
+vim.keymap.set("n", "J", "10j")
+vim.keymap.set("n", "K", "10k")
