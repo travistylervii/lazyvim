f8d371c69c7f99329227aca1bf5d6b6e5ffb896f
Travis <travis@netronline.com>
2024-02-21 10:20:17 -0700

    updating

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -5,6 +5,15 @@ return {
     dependencies = {
       "hrsh7th/cmp-emoji",
     },
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- change a keymap
+      --keys[#keys + 1] = { "K", "<cmd>echo 'hello'<cr>" }
+      -- disable a keymap
+      keys[#keys + 1] = { "K", false }
+      -- add a keymap
+      --keys[#keys + 1] = { "H", "<cmd>echo 'hello'<cr>" }
+    end,
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local cmp = require("cmp")
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -39,7 +39,7 @@ return {
         desc = "harpoon file",
       },
       {
-        "<C-b>",
+        "<C-k>",
         function()
           local harpoon = require("harpoon")
           harpoon.ui:toggle_quick_menu(harpoon:list())
